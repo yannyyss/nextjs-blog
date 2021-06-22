eb1142229e6140d6344b826499a68d5d645371c4
Yanely <yannyyss@hotmail.com>
2021-06-22 18:08:52 -0500

    nextjs blog example completed

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,24 @@
 import Layout from '../../components/layout'
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 // React component to render this page with postData
 // dangerouslySetInnerHTML is React’s replacement for using innerHTML in the browser DOM. In general, setting HTML from code is risky because it’s easy to inadvertently expose your users to a cross-site scripting (XSS) attack. 
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> 
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
@@ -21,11 +27,10 @@ export async function getStaticPaths () {
     
     // Return an array of possible value for id
     const paths = getAllPostIds()
-    console.log("paths",paths)
 
     return {
         paths,
-        fallback: false
+        fallback: false // If fallback is false, then any paths not returned by getStaticPaths will result in a 404 page.
     }
 
 }
