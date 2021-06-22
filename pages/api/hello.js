08b44d676744ca501f6a04766e0257e2e6127505
Yanely <yannyyss@hotmail.com>
2021-06-22 18:14:00 -0500

    api server-less function example for nextjs

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
+    const email = req.body.email //The API Route code will not be part of your client bundle, so you can safely write server-side code.
+    // Then save email to your database, etc...
+
     res.status(200).json({ text: 'Hello' })
 }
